b058f2f948d0d897ac5a95613c7d621de64a34d5
tamasubi <hidtamas@gmail.com>
2021-12-21 21:32:09 +0100

    crawling

---
+++ crawling.rb
@@ -0,0 +1,38 @@
+#! /usr/bin/env ruby
+
+require 'nokogiri'
+require 'open-uri'
+
+page = 1
+
+loop do
+    html = URI.open("http://github.com/rails/rails/pulls?page=#{page}")
+    
+    doc = Nokogiri::HTML(html)
+    
+    blank_icon = doc.css('.octicon.octicon-git-pull-request.blankslate-icon')
+
+    break if blank_icon
+
+    doc.css('a.no-underline.h4').each do |link|
+        puts link.content
+    end
+
+    page += 1
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
