49ca10fe84cf3798d6dbd87f717320e83d11ebb2
tamasubi <hidtamas@gmail.com>
2022-01-03 10:15:48 +0100

    without break

--- crawling.rb
+++ crawling.rb
@@ -9,13 +9,16 @@ loop do
     html = URI.open("http://github.com/rails/rails/pulls?page=#{page}")
     
     doc = Nokogiri::HTML(html)
-    
+
     blank_icon = doc.css('.octicon.octicon-git-pull-request.blankslate-icon')
 
-    break if blank_icon
+     
+    #break if blank_icon 
 
     doc.css('a.no-underline.h4').each do |link|
+        
         puts link.content
+
     end
 
     page += 1
