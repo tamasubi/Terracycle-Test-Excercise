96052aeae3fd2e9d398308a2e85ece6f06182e67
tamasubi <hidtamas@gmail.com>
2022-01-04 17:04:17 +0100

    list pr commits

--- crawling.rb
+++ crawling.rb
@@ -14,10 +14,23 @@ loop do
  
     break unless blank_icon.empty?
 
-    doc.css('a.no-underline.h4').each do |link|
-        
-        puts link.content
+    pr_links = doc.css('a.no-underline.h4')
 
+    pr_links.each do |link|
+        
+        pr_html = URI.open("http://github.com#{link["href"]}/commits")
+
+        pr_doc = Nokogiri::HTML(pr_html)
+
+        commit_links = pr_doc.css('a.tooltipped.tooltipped-sw.btn-outline.btn.BtnGroup-item.text-mono.f6') 
+
+        commit_links.each do |commit_link|
+
+            puts commit_link.content
+
+        end
+
+        puts '---'
     end
 
     page += 1
@@ -27,7 +40,8 @@ end
 
 
 
-
+#megnyitjuk a pullrequesteket, bemegyünk a commits fülbe, végigmegyünk az egyes commitokon, eltároljuk az egyes fájlok egyes sorainak változásait commitonként,
+# ahol duplikáció van az a pullrequest kiválasztásra kerül
 
 
 
